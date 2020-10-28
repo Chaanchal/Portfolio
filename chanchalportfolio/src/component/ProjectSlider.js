6d130a61d5830a4b85f22d8122ecfac67642c60f
Chaanchal <chaanchalagrawal@gmail.com>
2020-10-28 15:12:26 -0400

    ProjectSlider: added Next btn

--- chanchalportfolio/src/component/ProjectSlider.js
+++ chanchalportfolio/src/component/ProjectSlider.js
@@ -49,15 +49,6 @@ class ProjectSlider extends Component{
             //     this.slider.classList.remove("transfomer");
             // }
           
-            
-            
-            
-            
-            //debugger;
-            // if(slicedSlide){
-           
-            // }
-            
              setTimeout(() => {
                 //  debugger;
                 var slicedSlide = Array.prototype.slice.call(this.sliderArr, this.slideLength);
@@ -66,7 +57,7 @@ class ProjectSlider extends Component{
                 document.getElementsByClassName("active")[0].classList.remove("active");
                 this.slider.classList.remove("transfomer");
             
-            },300);
+            },100);
         }
     
         lastElem = () => {
@@ -77,23 +68,36 @@ class ProjectSlider extends Component{
 
     render(){ 
         return(
-            <div>
+            <div style={{display: "flex", flexDirection: "row" }}>
             <div id="slider-wrapp">
                 <div id="slider" className="_3D">
                     <div className="slide"><img src="https://preview.ibb.co/iHdNVn/1.jpg"/></div>
                     <div className="slide"><img src="https://preview.ibb.co/mUm9An/2.jpg"/></div>
                     <div className="slide"><img src="https://preview.ibb.co/hSL2Vn/3.jpg"/></div>
-                    {/* <div className="slide"><img src="https://preview.ibb.co/jRAfGS/4.jpg"/></div>
+                    <div className="slide"><img src="https://preview.ibb.co/jRAfGS/4.jpg"/></div>
                     <div className="slide"><img src="https://preview.ibb.co/ksTNVn/5.jpg"/></div>
-                    <div className="slide"><img src="https://preview.ibb.co/jQhhVn/6.jpg"/></div>
+                    {/* <div className="slide"><img src="https://preview.ibb.co/jQhhVn/6.jpg"/></div>
                     <div className="slide"><img src="https://preview.ibb.co/fD97wS/7.jpg"/></div>
                     <div className="slide"><img src="https://preview.ibb.co/c9jJ37/8.jpg"/></div>
                     <div className="slide"><img src="https://preview.ibb.co/iivEbS/9.jpg"/></div>
                     <div className="slide"><img src="https://preview.ibb.co/fXW9An/10.jpg"/></div> */}
                 </div>
-                <button onClick={ this.onClickNext } style={{margin: "auto"}}> Next </button>
+               
             </div>
+            <div>
+            <a className="nextBtn"onClick={ this.onClickNext } > <i class="fa fa-chevron-right fa-3x"></i> </a>
+              {/* ADD LATER style={{margin: "auto", position:"relative", color: "white"}} */}
+                <div className="projName" style={{backgroundColor: "blue"}}>
+                    Project_NAME
+                </div>
+
+                <div className="projData" style={{backgroundColor: "red"}}>
+                Project_Data
+               
+                </div>
             
+          
+            </div>
             </div>
         );
     }
